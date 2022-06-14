9ff0c2b53d85da30ddfec8c6151699d297944c95
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-14 11:09:29 -0500

    answered questions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Brandon Moyer");
+Console.WriteLine("Brookfield Central");
+Console.WriteLine("A geology kit that I'll never be able to use");
