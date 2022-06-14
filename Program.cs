cc97d20b338462d212724f17e3d88e9bc3481471
Surya Arvind <24arvins@elmbrookstudents.org>
2022-06-14 11:11:39 -0500

    CSharpActivity

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Surya Arvind");
+Console.WriteLine("Brookfield Central High School");
+Console.WriteLine("A gift card");
